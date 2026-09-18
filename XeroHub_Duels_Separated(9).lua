1c26e27060e96faf768f8215733d4f5833fbb26c
sanxsmov <sanxsmov@gmail.com>
2026-09-18 14:43:46 -0600

    Add files via upload

--- XeroHub_Duels_Separated(9).lua
+++ XeroHub_Duels_Separated(9).lua
@@ -2,7 +2,7 @@
 
 local SoundCatalogCore = {
     TARGET_SOUND_ID = "10209603",
-    CATALOG_URL = "https://api.github.com/repos/sanxsmov/Mis-soundsp/contents/sounds/monsterkill-famale.mp3?ref=main",
+    CATALOG_URL = "https://github.com/sanxsmov/Mis-soundsp/tree/814697752be460c105a661bb04c080fb8b924d7c/sounds",
     CACHE_FOLDER = "XeroHub/SoundsV2",
 }
 
